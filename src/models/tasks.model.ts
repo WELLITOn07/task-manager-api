3fd03170cd480288dcc3ab9b0823b78254b099f9
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2023-04-18 19:05:02 -0300

    feat(repositories): create task repository

--- src/models/tasks.model.ts
+++ src/models/tasks.model.ts
@@ -1,6 +1,23 @@
-export interface Task {
-    id: string,
-    title: string,
-    description: string,
-    completed: boolean,
-};
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+
+export const taskSchema = new Schema({
+    id: {
+        type: String
+    },
+    title: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+export const Task = mongoose.model('Task', taskSchema);
---
+++ src/repositories/task.repository.ts
@@ -0,0 +1,25 @@
+import { Task } from './../models/tasks.model';
+
+class TaskRepository {
+    getAll() {
+       return Task.find();
+    }
+
+    getById(id: string) {
+        return Task.findOne({id});
+    }
+
+    create(task: typeof Task) {
+        return Task.create(task);
+    }
+
+    update(id: string, task: typeof Task) {
+        return Task.updateOne({id}, {$set: task})
+    }
+
+    remove(id: string) {
+        return Task.deleteOne({id});
+    }
+};
+
+export default new TaskRepository();
