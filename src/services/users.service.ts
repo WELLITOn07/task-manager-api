5969b1442bd072b3fb9430bfebfc4b046050bf92
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2023-04-19 00:26:35 -0300

    feat(users): create users service

---
+++ src/services/users.service.ts
@@ -0,0 +1,48 @@
+import { User } from "../models/user.model";
+import userRepository from "../repositories/user.repository";
+
+class UsersService {
+  async getAll() {
+    const users = await userRepository.getAll();
+
+    return users;
+  }
+
+  async getById(email: string) {
+    const user = await userRepository.getByEmail(email);
+
+    return user;
+  }
+
+  async create(user: typeof User) {
+    const createdUser = await userRepository.create(user);
+
+    if (!createdUser) {
+      throw new Error();
+    }
+
+    return createdUser;
+  }
+
+  async remove(email: string) {
+    const deletedUser = await userRepository.remove(email);
+
+    if (deletedUser.deletedCount <= 0) {
+      throw new Error();
+    }
+
+    return deletedUser;
+  }
+
+  async update(email: string, user: Partial<typeof User>) {
+    const updatedUser = await userRepository.update(email, user);
+
+    if (updatedUser.matchedCount <= 0) {
+      throw new Error();
+    }
+
+    return updatedUser;
+  }
+}
+
+export default new UsersService();
