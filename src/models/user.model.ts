ede9f1053a10bbb0ff82d5a4450ded9248dd736e
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2023-04-19 20:35:02 -0300

    fix(user-model): reajusted model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,16 +2,12 @@ import { Schema } from "mongoose";
 import mongoose from "mongoose";
 
 export interface IUser {
-  _id?: string;
   email: string;
   password: string;
-  createdAt: string | Date
+  createdAt?: string | Date
 };
 
 export const userSchema = new Schema<IUser>({
-  _id: {
-    type: String,
-  },
   email: {
     type: String
   },
